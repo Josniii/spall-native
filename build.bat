4d059eca2ff03c519753bbc8299d4df09417b4a5
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2022-11-14 05:20:58 -0500

    Build script options

--- build.bat
+++ build.bat
@@ -5,7 +5,11 @@ md bin
 copy "..\Odin\vendor\sdl2\SDL2.dll" "bin\SDL2.dll"
 copy "..\Odin\vendor\sdl2\ttf\SDL2_ttf.dll" "bin\SDL2_ttf.dll"
 copy "fonts\*.*" "bin"
-REM odin build src -subsystem:windows -collection:formats=formats -out:bin\spall.exe -no-bounds-check -o:speed
-REM odin build src -collection:formats=formats -out:bin\spall.exe -no-bounds-check -o:speed
-REM odin build src -collection:formats=formats -out:bin\spall.exe -no-bounds-check -debug -o:speed -keep-temp-files
-odin build src -collection:formats=formats -out:bin\spall.exe -debug -o:speed -keep-temp-files
+
+if "%1"=="release" (
+    odin build src -collection:formats=formats -out:bin\spall.exe -debug -o:speed -no-bounds-check -subsystem:windows -define:GL_DEBUG=false
+) else if "%1"=="opt" (
+    odin build src -collection:formats=formats -out:bin\spall.exe -debug -o:speed
+) else (
+    odin build src -collection:formats=formats -out:bin\spall.exe -debug -keep-temp-files
+)
