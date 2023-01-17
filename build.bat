16fb4e5b7853bad586fdbee0f7460c1235189e65
Colin Davidson <colrdavidson@gmail.com>
2023-01-16 22:30:18 -0800

    fix win-build

--- build.bat
+++ build.bat
@@ -2,9 +2,6 @@
 rmdir /s /q bin
 md bin
 
-copy "..\Odin\vendor\sdl2\SDL2.dll" "bin\SDL2.dll"
-copy "..\Odin\vendor\sdl2\ttf\SDL2_ttf.dll" "bin\SDL2_ttf.dll"
-
 if "%1"=="release" (
     odin build src -collection:formats=formats -out:bin\spall.exe -debug -o:speed -no-bounds-check -subsystem:windows -define:GL_DEBUG=false
 ) else if "%1"=="opt" (
