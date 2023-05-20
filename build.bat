28eca02d5368b6ddb340a4a319278e1f61af0892
Colin Davidson <colrdavidson@gmail.com>
2023-05-20 06:36:40 -0700

    update build-script to include icon

--- build.bat
+++ build.bat
@@ -3,9 +3,9 @@ rmdir /s /q bin
 md bin
 
 if "%1"=="release" (
-    odin build src -collection:formats=formats -out:bin\spall.exe -debug -o:speed -no-bounds-check -subsystem:windows -define:GL_DEBUG=false
+    odin build src -collection:formats=formats -out:bin\spall.exe -debug -o:speed -no-bounds-check -subsystem:windows -define:GL_DEBUG=false -resource:resources\spall.rc
 ) else if "%1"=="opt" (
-    odin build src -collection:formats=formats -out:bin\spall.exe -debug -o:speed
+    odin build src -collection:formats=formats -out:bin\spall.exe -debug -o:speed -resource:resources\spall.rc
 ) else (
-    odin build src -collection:formats=formats -out:bin\spall.exe -debug -keep-temp-files
+    odin build src -collection:formats=formats -out:bin\spall.exe -debug -keep-temp-files -resource:resources\spall.rc
 )
