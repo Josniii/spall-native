1aa340586355152617c3a21df9d6b7a1e5e44a3c
Colin Davidson <colrdavidson@gmail.com>
2024-07-05 17:15:14 -0700

    push minimum version back

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ rm -rf bin
 mkdir bin
 
 if [ "$1" = "release" ]; then
-	odin build src -collection:formats=formats -out:bin/spall -debug -o:speed -no-bounds-check -define:GL_DEBUG=false -strict-style
+	odin build src -collection:formats=formats -out:bin/spall -debug -o:speed -no-bounds-check -define:GL_DEBUG=false -strict-style -minimum-os-version:11.0
 elif [ "$1" = "opt" ]; then
-	odin build src -collection:formats=formats -out:bin/spall -debug -o:speed -strict-style
+	odin build src -collection:formats=formats -out:bin/spall -debug -o:speed -strict-style -minimum-os-version:11.0
 else
-	odin build src -collection:formats=formats -out:bin/spall -debug -keep-temp-files -strict-style
+	odin build src -collection:formats=formats -out:bin/spall -debug -keep-temp-files -strict-style -minimum-os-version:11.0
 fi
